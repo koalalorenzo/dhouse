58b0b0104d567acb6387a4e077224dc2a9c3cd68
maxscience <maxscience@gmail.com>
2012-12-01 13:02:14 +0100

    Added common.py with IP-based geolocation

---
+++ chouse/common.py
@@ -0,0 +1,28 @@
+import urllib2
+from xml.dom import minidom
+
+#IP-based Geolocation
+IP_URL ="http://freegeoip.net/xml/"
+def get_coords(ip):
+    url = IP_URL + ip
+    content = None
+    try:
+        content = urllib2.urlopen(url).read()
+    except URLError: #no coordinates
+        return
+    logging.info("GETCOORDS")
+    logging.info(content)
+    if content:
+        #parse the xml and find the coordinates
+        g = minidom.parseString(content)
+        lat = g.getElementsByTagName('Latitude')[0].childNodes[0].nodeValue
+        lon = g.getElementsByTagName('Longitude')[0].childNodes[0].nodeValue
+        logging.info(lat)
+        return db.GeoPt(lat, lon) #GAE datatype for coordinates
+
+#give the coordinates to Gmap
+GMAPS_URL = "http://maps.googleapis.com/maps/api/staticmap?size=380x263&sensor=false&"
+def gmaps_img(points):
+    markers = '&'.join('markers=%s,%s' % (p.lat, p.lon)
+    for p in points:
+    	return GMAPS_URL + markers
