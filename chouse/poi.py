588395e9a159520c6068c6629772b5fbe76819ef
Lorenzo Setale <koalalorenzo@gmail.com>
2012-12-01 13:11:58 +0100

    Improved API integrations with Foursquare

--- chouse/poi.py
+++ chouse/poi.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 import json
 import foursquare
-from chouse.metweet import *
+from chouse.metwit import *
 from chouse.conf import *
+from django.utils.encoding import smart_str
 
 class Poi(object):
     def __init__(self):
@@ -25,46 +26,79 @@ class Poi(object):
         venues = fsq.venues.search({
                                     "ll":"%s,%s" % (self.cordinates['x'], self.cordinates['y']),
                                     "limit": 50,
-                                    "radius": 800,
+                                    "radius": "1000",
                                     "intent": "browse",
-                                    "near": "Roma"
                                     })['venues']
-        gren = 25
+        green = 25
         times = 1
         near_place = dict()
         near_place['location'] = dict()
         near_place['location']['distance'] = 0
+        near_place['referralId'] = None
+        near_place['name'] = ""
+        
+        near_station = dict()
+        near_station['location'] = dict()
+        near_station['location']['distance'] = 0
+        near_station['referralId'] = None
+        near_station['name'] = ""
+        
+        near_plaza = dict()
+        near_plaza['location'] = dict()
+        near_plaza['location']['distance'] = 0
+        near_plaza['referralId'] = None
+        near_plaza['name'] = ""
         
         for venue in venues:
-            if "station" in venue['name'].lower() or "bus" in venue['name'].lower():
-                green += 75
-                times += 1
-                if int(near_place['location']['distance']) < int(venue['location']['distance']):
-                    near_place = venue
-                continue
-                
+            print smart_str(venue['name']), ":"
             is_green = False
             for category in venue['categories']:
-                if "plaza" in category['shortName'] or "green" in category['shortName']:
-                    green +=75
+                print smart_str(category['shortName'])
+                s_category = category['shortName'].lower()
+                
+                if "plaza" in s_category or "outdoor" in s_category:
+                    green +=80
+                    times += 1
+                    if int(near_plaza['location']['distance']) < int(venue['location']['distance']):
+                        self.analysis_data['Outdoor place'] = "( %sm ) %s" % ( venue['location']['distance'], venue['name'])
+                        near_plaza = venue
+                    is_green = True
+                    
+                elif "historyc" in s_category or "fountain" in s_category:
+                    green +=80
                     times += 1
                     is_green = True
                         
-                if "station" in category['shortName'] or "bus" in category['shortName']:
+                elif "station" in s_category or "bus" in s_category:
                     green +=75
                     times += 1
+                    if int(near_station['location']['distance']) < int(venue['location']['distance']):
+                        self.analysis_data['Public station'] = "( %sm ) %s" % ( venue['location']['distance'], venue['name'])
+                        near_station = venue
                     is_green = True
-                    
-                if "Historyc" in category['shortName'] or "bus" in category['shortName']:
+
+                elif "subway" in s_category or "metro" in s_category.lower():
                     green +=75
                     times += 1
+                    if int(near_station['location']['distance']) < int(venue['location']['distance']):
+                        self.analysis_data['Public station'] = "( %sm ) %s" % ( venue['location']['distance'], venue['name'])
+                        near_station = venue
                     is_green = True
+
+                elif "school" in s_category or "univer" in s_category.lower():
+                    green +=70
+                    times += 1
+                break
+                
+            print "----"
 
             if is_green:
                 if int(near_place['location']['distance']) < int(venue['location']['distance']):
-                    near_place = venue
-        
-        self.analysis_data['Greener place near'] = "( %sm ) %s" % ( near_place['location']['distance'], near_place['name'])
+                    if venue["referralId"] != near_station['referralId'] or venue['referralId'] != near_plaza['referralId']:
+                        near_place = venue
+    
+        if not near_place['name'] == "":
+            self.analysis_data['Greener place near'] = "( %sm ) %s" % ( near_place['location']['distance'], near_place['name'])
         
         return int(green/times) # Media
         
