4030a0f8e87f91baf9c42011e0a070f194a5bb02
Lorenzo Setale <koalalorenzo@gmail.com>
2012-12-02 12:37:25 +0100

    Fixed problems and implemented descriptions and data in Point view

--- daata.py
+++ daata.py
@@ -16,24 +16,20 @@ uni.database = db
 uni.title = "Open Space Roma 3"
 uni.cap = "00145"
 uni.energetic_class = "d"
+uni.link = "http://www.uniroma3.it/"
+uni.description = "Deep inside the most important university of Rome, this place is really important for startups and ideas"
 
 enlabs.title = "Bellissimo Open Space Termini"
 enlabs.cap = "00185"
 enlabs.energetic_class = "e"
+enlabs.link = "http://enlabs.it"
+enlabs.description = "Cool place for startups"
 
-enlabs.photos.append({
-                        "url":"https://dl.dropbox.com/u/1145876/enlabs/open/1.JPG",
-                        "description":"wide area",
-                        "title": "Wide Area"    
-                    })
+enlabs.photos.append({ "url":"https://dl.dropbox.com/u/1145876/enlabs/open/1.JPG", "description":"wide area", "title": "Wide Area" })
 
-enlabs.photos.append({
-                        "url":"https://dl.dropbox.com/u/1145876/enlabs/open/2.JPG",
-                        "description":"wide area",
-                        "title": "Wide Area"    
-                    })
+enlabs.photos.append({ "url":"https://dl.dropbox.com/u/1145876/enlabs/open/2.JPG",  "description":"wide area", "title": "Wide Area" })
                     
-enlabs.photos.append({"url":"https://dl.dropbox.com/u/1145876/enlabs/open/3.JPG", "description":"wide area","title": "Wide Area" })
+enlabs.photos.append({ "url":"https://dl.dropbox.com/u/1145876/enlabs/open/3.JPG", "description":"wide area","title": "Wide Area" })
                     
 
 enlabs.calculate_value()
