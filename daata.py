e326716586aa8e548d31f357494f4580d1cd2a35
Lorenzo Setale <koalalorenzo@gmail.com>
2012-12-02 01:49:45 +0100

    Improved lat long

--- daata.py
+++ daata.py
@@ -5,9 +5,9 @@ import foursquare
 foursquare_client = foursquare.Foursquare(client_id=FOURSQUARE_API_CLIENT_ID, client_secret=FOURSQUARE_API_CLIENT_SECRET)
 
 
-enlabs = Poi("41.90464064950719", "12.499051094055176")
+enlabs = Poi("41.89954600592677", "12.502334117889404")
 metro_marconi = Poi("41.8493344", "12.4755884")
-uni = Poi("41.8562461", "12.4688934")
+uni = Poi("41.85556186229819", "12.470297813415527")
 
 enlabs.get_foursquare_vote(foursquare_client)
 enlabs.analysis_data
